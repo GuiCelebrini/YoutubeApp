470fc06c5667b79819ad4d5953011eb60df8177d
GuiCelebrini <celebrinig@gmail.com>
2021-12-20 12:57:54 -0300

    Updating the toolbar and setting a RecyclerView in MainActivity

--- app/src/main/java/com/android/guicelebrini/youtubeapp/MainActivity.kt
+++ app/src/main/java/com/android/guicelebrini/youtubeapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.android.guicelebrini.youtubeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        configureToolbar()
+    }
+
+    private fun configureToolbar(){
+        var toolbarMain : Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbarMain)
     }
 }
